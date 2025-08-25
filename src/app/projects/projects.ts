a8bcc8ba4ea188c2a3a388ca259358ca5d0177b4
Johan <cjvv_5730@hotmail.com>
2025-08-24 19:12:10 -0500

    Manejo de errores llamada de la API

--- src/app/projects/projects.ts
+++ src/app/projects/projects.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 export interface Project {
   id: number;
@@ -12,12 +13,17 @@ export interface Project {
   providedIn: 'root'
 })
 export class ProjectsService {
-
   private API_URL = 'https://jsonplaceholder.typicode.com/users';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getProjects(): Observable<Project[]> {
-    return this.http.get<Project[]>(this.API_URL);
+    return this.http.get<Project[]>(this.API_URL).pipe(
+      catchError((error) => {
+        console.error('Error al obtener proyectos:', error);
+        return throwError(() => new Error('No se pudieron cargar los proyectos.'));
+      })
+    );
   }
 }
+
--- src/app/tasks/tasks.ts
+++ src/app/tasks/tasks.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,11 @@ export class TasksService {
   constructor(private http: HttpClient) {}
 
   getTasksByProject(projectId: number): Observable<any[]> {
-    return this.http.get<any[]>(`${this.apiUrl}?userId=${projectId}`);
+    return this.http.get<any[]>(`${this.apiUrl}?userId=${projectId}`).pipe(
+      catchError((error) => {
+        console.error('Error al obtener las tareas:', error);
+        return throwError(() => new Error('No se pudieron cargar las tareas.'));
+      })
+    );
   }
 }
